097d150baea3c2b94d5bd4d50f1173b25ebed551
Marc Jou <smyslov@gmail.com>
2016-08-09 23:10:32 +0200

    Understanding template system

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -10,15 +10,10 @@ import play.api.mvc._
  */
 @Singleton
 class HomeController @Inject() extends Controller {
-
-  /**
-   * Create an Action to render an HTML page with a welcome message.
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
   def index = Action {
-    Ok(views.html.index("Hello ScalaWorld"))
+    val section_title = "Hello ScalaWorld"
+    val simple_list = List(1, 2, 3, 4)
+    Ok(views.html.index(section_title, simple_list))
   }
 
 }
